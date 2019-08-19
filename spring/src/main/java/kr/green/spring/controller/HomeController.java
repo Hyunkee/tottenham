f6b078b2da930158c9e6103542a391d6edb91af4
hyunkee <khk2909@daum.net>
2019-08-19 14:47:51 +0900

    19. 08. 19 컨트롤러 로그인페이지, 회원가입페이지 연결

--- spring/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring/src/main/java/kr/green/spring/controller/HomeController.java
@@ -2,10 +2,14 @@ package kr.green.spring.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.spring.service.MemberService;
+import kr.green.spring.vo.MemberVO;
 
 /**
  * Handles requests for the application home page.
@@ -13,12 +17,33 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	MemberService memberService;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);	
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView mainGet(ModelAndView mv) throws Exception{		
-	    mv.setViewName("/main/home");
-	    mv.addObject("setHeader", "타일즈");
+	    mv.setViewName("/main/home");	    
 	    return mv;
+	}
+	@RequestMapping(value="/member/signin", method=RequestMethod.GET)
+	public ModelAndView signinGet(ModelAndView mv) throws Exception{		
+	    mv.setViewName("/member/signin");	    
+	    return mv;
+	}
+	@RequestMapping(value="/member/signin", method=RequestMethod.POST)
+	public String signinPost(ModelAndView mv) throws Exception{	    	    
+	    return "redirect:/";
+	}
+	@RequestMapping(value="/member/register", method=RequestMethod.GET)
+	public ModelAndView registerGet(ModelAndView mv) throws Exception{		
+	    mv.setViewName("/member/register");	    
+	    return mv;
+	}
+	@RequestMapping(value="/member/register", method=RequestMethod.POST)
+	public String registerPost(MemberVO mVo){
+		
+		return "redirect:/";	    
 	}
 }
