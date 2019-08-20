cbeb54e4880dbe947addc2dc05b8396c562f6b07
hyunkee <khk2909@daum.net>
2019-08-20 10:11:43 +0900

    19. 08. 20  컨트롤러, VO 수정

--- spring/src/main/java/kr/green/tottenham/controller/HomeController.java
+++ spring/src/main/java/kr/green/tottenham/controller/HomeController.java
@@ -1,19 +1,20 @@
-package kr.green.spring.controller;
+package kr.green.tottenham.controller;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import kr.green.spring.service.MemberService;
-import kr.green.spring.vo.MemberVO;
+import kr.green.tottenham.service.MemberService;
+import kr.green.tottenham.vo.MemberVO;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
@@ -33,7 +34,19 @@ public class HomeController {
 	    return mv;
 	}
 	@RequestMapping(value="/member/signin", method=RequestMethod.POST)
-	public String signinPost(ModelAndView mv) throws Exception{	    	    
+	public String signinPost(MemberVO mVo, Model model){
+		logger.info("로그인 진행중");
+		System.out.println(mVo);
+		MemberVO user = memberService.signin(mVo);
+		if(user != null) {
+			model.addAttribute("user",user);
+			return "redirect:/";
+		}
+		return "redirect:/member/signin";	    
+	}
+	@RequestMapping(value="/member/signout", method=RequestMethod.GET)
+	public String signoutGet(HttpServletRequest r) throws Exception{
+		r.getSession().removeAttribute("user");
 	    return "redirect:/";
 	}
 	@RequestMapping(value="/member/register", method=RequestMethod.GET)
@@ -43,7 +56,11 @@ public class HomeController {
 	}
 	@RequestMapping(value="/member/register", method=RequestMethod.POST)
 	public String registerPost(MemberVO mVo){
-		
-		return "redirect:/";	    
+		System.out.println(mVo);
+		if(memberService.register(mVo))
+			return "redirect:/";
+		else {
+			return "redirect:/member/register";
+		}		
 	}
 }
